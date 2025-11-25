e53e9be92e7e2d48b8a8ad039dbf740cb7624867
Pablo Benítez <pablo.ben.037@gmail.com>
2025-11-25 00:47:13 -0600

    Update FairFaceDatasetZip.py

--- Atención y modelos generativos/FairFaceDatasetZip.py
+++ Atención y modelos generativos/FairFaceDatasetZip.py
@@ -4,23 +4,15 @@ import os, PIL.Image
 class FairFaceDatasetZip(Dataset):
     def __init__(self, df, root_dir, transform=None):
         self.df = df.reset_index(drop=True)
-        self.root_dir = root_dir      # carpeta donde están las imágenes
+        self.root_dir = root_dir      
         self.transform = transform
 
     def __getitem__(self, idx):
-        # nombre del archivo dentro del DF
         file_name = self.df.loc[idx, "file"]
 
-        # ruta completa a la imagen
         img_path = os.path.join(self.root_dir, file_name)
-
-        # abrir imagen
         img = PIL.Image.open(img_path).convert("RGB")
-
-        # etiqueta
         cat = self.df.loc[idx, "age_CG"]
-
-        # aplicar transformaciones si hay
         if self.transform:
             img = self.transform(img)
 
